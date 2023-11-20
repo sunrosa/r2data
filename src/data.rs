9e09111597ed7fb2d92bf9a7cab9b62f389a33ef
Ibis Sunrosa <drsunrosa@gmail.com>
2023-11-20 05:17:08 +0000

    Impl Display for Item

--- src/data.rs
+++ src/data.rs
@@ -635,6 +635,12 @@ impl From<ItemId> for Item {
     }
 }
 
+impl std::fmt::Display for Item {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum OutcomeId {
     Defeat,
